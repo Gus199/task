47127fa3f34cca512629715f2c8a0b686293bb72
Amar <augustine2903a@gmail.com>
2022-11-15 21:13:41 -0500

    remove one line

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,6 @@ function Home() {
   return (
     <>
       <section className='heading'>
-        <h1>What do you need help with?</h1>
         <p>Please choose from an option below</p>
       </section>
 
